93173a5aadb4c9273e3a3711c407aa818161bb4d
Betty van Aken <bettyvanaken@gmail.com>
2019-04-26 15:56:05 +0200

    run ner and coref experiments

--- probing/probe_all_layers.sh
+++ probing/probe_all_layers.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-function run_task() {
+function run_ner_task() {
 
     python -u /app/main.py --config_file /app/config/edgeprobe_bert_babi.conf -o \
     "target_tasks=layers-ner-ontonotes,exp_name=bert_babi_ner_layer_$1,bert_embedding_layer=$1"
 }
 
-for i in 1 10
+function run_coref_task() {
+
+    python -u /app/main.py --config_file /app/config/edgeprobe_bert_babi.conf -o \
+    "target_tasks=layers-coref-ontonotes,exp_name=bert_babi_coref_layer_$1,bert_embedding_layer=$1"
+}
+
+for i in {0..10}
 do
-   run_task $i
+   run_ner_task $i
 done
 
+for j in {0..10}
+do
+   run_coref_task $j
+done
