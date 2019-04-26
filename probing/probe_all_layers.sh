e1eaafdd4b55176dafea7a7e0a8cfb88ee6fc2ed
Betty van Aken <bettyvanaken@gmail.com>
2019-04-26 12:00:24 +0200

    fix probe_all_layers.sh syntax

--- probing/probe_all_layers.sh
+++ probing/probe_all_layers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-function run_task(layer) {
+function run_task() {
 
-    python -u ../main.py --config_file ../config/edgeprobe_bert_babi.conf -o \
-    "target_tasks=layers-ner-ontonotes,exp_name=bert_babi_ner_layer_$layer,layer=$layer"
+    python -u /app/main.py --config_file /app/config/edgeprobe_bert_babi.conf -o \
+    "target_tasks=layers-ner-ontonotes,exp_name=bert_babi_ner_layer_$1,bert_embedding_layer=$1"
 }
 
 for i in 1 10
 do
-   run_task($i)
+   run_task $i
 done
 
