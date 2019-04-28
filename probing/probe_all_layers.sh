ea8fbd2e5393089b2523c970975bcd3b70566d46
Betty van Aken <bettyvanaken@gmail.com>
2019-04-29 00:00:45 +0200

    add semeval rel task to probe_all_layers.sh

--- probing/probe_all_layers.sh
+++ probing/probe_all_layers.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
-# function run_ner_task() {
-
-#     python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
-#     "target_tasks=layers-ner-ontonotes, \
-#     bert_model_name=bert-large-uncased, \
-#     exp_name=bert_hotpot_ner_layer_$1, \
-#     bert_embedding_layer=$1, \
-#     bert_model_file=/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
-# }
-
-# function run_coref_task() {
-
-#     python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
-#     "target_tasks=layers-coref-ontonotes, \
-#     bert_model_name=bert-large-uncased, \
-# 	exp_name=bert_hotpot_ner_layer_$1, \
-#     bert_embedding_layer=$1, \
-#     bert_model_file=/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
-# }
-
-function run_coref_task_hotpot() {
+function run_ner_task() {
 
     python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
-    "target_tasks=layers-coref-ontonotes, \
+    "target_tasks=layers-ner-ontonotes, \
     bert_model_name=bert-large-uncased, \
-	exp_name=bert_hotpot_coref_layer_$1, \
+    exp_name=bert_hotpot_ner_layer_$1, \
     bert_embedding_layer=$1, \
     bert_model_file=/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
 }
@@ -35,12 +15,20 @@ function run_coref_task() {
     python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
     "target_tasks=layers-coref-ontonotes, \
     bert_model_name=bert-large-uncased, \
-	exp_name=bert_unfinetuned_coref_layer_$1, \
-    bert_embedding_layer=$1"
+	exp_name=bert_hotpot_ner_layer_$1, \
+    bert_embedding_layer=$1, \
+    bert_model_file=/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
 }
 
-run_coref_task_hotpot 10
-run_coref_task 10
+function run_rel_semeval_task() {
+
+    python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
+    "target_tasks=edges-rel-semeval, \
+    bert_model_name=bert-large-uncased, \
+	exp_name=bert_hotpot_rel_layer_$1, \
+    bert_embedding_layer=$1, \
+    bert_model_file=/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
+}
 
 # for i in 1 3 5 7 9 11 13 15 17 19 21 23
 # do
@@ -51,3 +39,8 @@ run_coref_task 10
 # do
 #    run_coref_task $j
 # done
+
+for j in 1 3 5 7 9 11 13 15 17 19 21 23
+do
+   run_rel_semeval_task $j
+done
