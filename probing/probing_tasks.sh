ab6f6b62908f069efccd933c81b1d8d6a63ab1e2
Betty van Aken <bettyvanaken@gmail.com>
2019-05-03 18:32:57 +0200

    add missing tasks, delete old ones

--- probing/probing_tasks.sh
+++ probing/probing_tasks.sh
@@ -60,13 +60,69 @@ function run_sup_babi2_task() {
     max_epochs=10"
 }
 
-function qtype_babi_layers() {
+function qtype_hotpot() {
+    BERT_TYPE="bert-large-uncased"
+    MODEL_FILE="/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
+    EXP_NAME="hotpot"
+    for i in {0..23}
+    do
+       run_qtype_task $i $BERT_TYPE $MODEL_FILE $EXP_NAME 0
+    done
+}
+
+function qtype_nofinetune_base() {
     BERT_TYPE="bert-base-uncased"
-    MODEL_FILE="/data_dir/bert_models/babi/qa21_base/pytorch_model.bin"
-    EXP_NAME="babi"
+
     for i in {0..11}
     do
-       run_qtype_task $i $BERT_TYPE $MODEL_FILE $EXP_NAME 1
+        python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
+        "target_tasks=layers-qtype-trec, \
+        bert_embedding_layer=$i, \
+        bert_model_name=$BERT_TYPE, \
+        exp_name=bert_nofinetune_base_qtype_layer_$i, \
+        max_epochs=10"
+    done
+}
+
+function qtype_nofinetune_large() {
+    BERT_TYPE="bert-large-uncased"
+
+    for i in {0..23}
+    do
+        python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
+        "target_tasks=layers-qtype-trec, \
+        bert_embedding_layer=$i, \
+        bert_model_name=$BERT_TYPE, \
+        exp_name=bert_nofinetune_large_qtype_layer_$i, \
+        max_epochs=10"
+    done
+}
+
+function sup_babi2_nofinetune_base() {
+    BERT_TYPE="bert-base-uncased"
+
+    for i in {0..11}
+    do
+        python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
+        "target_tasks=layers-sup-babi2, \
+        bert_embedding_layer=$i, \
+        bert_model_name=$BERT_TYPE, \
+        exp_name=bert_nofinetune_sup_babi2_layer_$i, \
+        max_epochs=10"
+    done
+}
+
+function sup_babi2_nofinetune_large() {
+    BERT_TYPE="bert-large-uncased"
+
+    for i in {0..23}
+    do
+        python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
+        "target_tasks=layers-sup-babi2, \
+        bert_embedding_layer=$i, \
+        bert_model_name=$BERT_TYPE, \
+        exp_name=bert_nofinetune_large_sup_babi2_layer_$i, \
+        max_epochs=10"
     done
 }
 
@@ -80,7 +136,17 @@ function sup_babi2_babi() {
     done
 }
 
-function ner_babi_layers() {
+function sup_babi2_hotpot() {
+    BERT_TYPE="bert-large-uncased"
+    MODEL_FILE="/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
+    EXP_NAME="hotpot"
+    for i in {0..23}
+    do
+       run_sup_babi2_task $i $BERT_TYPE $MODEL_FILE $EXP_NAME 0
+    done
+}
+
+function ner_babi() {
     BERT_TYPE="bert-base-uncased"
     MODEL_FILE="/data_dir/bert_models/babi/qa21_base/pytorch_model.bin"
     EXP_NAME="babi"
@@ -110,37 +176,7 @@ function rel_babi_layers() {
     done
 }
 
-function ner_hotpot_layers() {
-    BERT_TYPE="bert-large-uncased"
-    MODEL_FILE="/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
-    EXP_NAME="hotpot"
-    for i in {0..23}
-    do
-       run_ner_task $i $BERT_TYPE $MODEL_FILE $EXP_NAME 0
-    done
-}
-
-function coref_hotpot_layers() {
-    BERT_TYPE="bert-large-uncased"
-    MODEL_FILE="/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
-    EXP_NAME="hotpot"
-    for i in {0..23}
-    do
-       run_coref_task $i $BERT_TYPE $MODEL_FILE $EXP_NAME 0
-    done
-}
-
-function rel_hotpot_layers() {
-    BERT_TYPE="bert-large-uncased"
-    MODEL_FILE="/data_dir/bert_models/hotpot_small_distract/pytorch_model.bin"
-    EXP_NAME="hotpot"
-    for i in {0..23}
-    do
-       run_rel_semeval_task $i $BERT_TYPE $MODEL_FILE $EXP_NAME 0
-    done
-}
-
-function ner_nofinetune_layers() {
+function ner_nofinetune_large() {
     BERT_TYPE="bert-large-uncased"
 
     for i in {0..23}
@@ -154,7 +190,7 @@ function ner_nofinetune_layers() {
     done
 }
 
-function coref_nofinetune_layers() {
+function coref_nofinetune_large() {
     BERT_TYPE="bert-large-uncased"
 
     for i in {0..23}
@@ -168,72 +204,125 @@ function coref_nofinetune_layers() {
     done
 }
 
-function rel_nofinetune_layers() {
-    BERT_TYPE="bert-large-uncased"
+function rel_nofinetune_base() {
+    BERT_TYPE="bert-base-uncased"
 
-    for i in {0..23}
+    for i in {0..11}
     do
         python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
         "target_tasks=edges-rel-semeval, \
         bert_embedding_layer=$i, \
         bert_model_name=$BERT_TYPE, \
-        exp_name=bert_nofinetune_rel_layer_$i, \
+        exp_name=bert_base_nofinetune_rel_layer_$i, \
         max_epochs=10"
     done
 }
 
-if [ $1 == 'ner_babi_layers' ]
+function ner_nofinetune_base() {
+    BERT_TYPE="bert-base-uncased"
+
+    for i in {0..11}
+    do
+        python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
+        "target_tasks=layers-ner-ontonotes, \
+        bert_embedding_layer=$i, \
+        bert_model_name=$BERT_TYPE, \
+        exp_name=bert_base_nofinetune_ner_layer_$i, \
+        max_epochs=10"
+    done
+}
+
+function coref_nofinetune_base() {
+    BERT_TYPE="bert-base-uncased"
+
+    for i in {0..11}
+    do
+        python -u /app/main.py --config_file /app/config/edgeprobe_bert_finetuned.conf -o \
+        "target_tasks=layers-coref-ontonotes, \
+        bert_embedding_layer=$i, \
+        bert_model_name=$BERT_TYPE, \
+        exp_name=bert_base_nofinetune_coref_layer_$i, \
+        max_epochs=10"
+    done
+}
+
+### check layer 5:
+if [ $1 == 'rel_babi_layers' ]
 then
-ner_babi_layers
+rel_babi_layers
 fi
+
+### running:
 
 if [ $1 == 'coref_babi_layers' ]
 then
 coref_babi_layers
 fi
 
-if [ $1 == 'rel_babi_layers' ]
+if [ $1 == 'coref_nofinetune_large' ]
 then
-rel_babi_layers
+coref_nofinetune_large
 fi
 
-if [ $1 == 'ner_hotpot_layers' ]
+if [ $1 == 'ner_nofinetune_large' ]
 then
-ner_hotpot_layers
-fi
-
-if [ $1 == 'coref_hotpot_layers' ]
-then
-coref_hotpot_layers
-fi
-
-if [ $1 == 'rel_hotpot_layers' ]
-then
-rel_hotpot_layers
-fi
-
-if [ $1 == 'ner_nofinetune_layers' ]
-then
-ner_nofinetune_layers
-fi
-
-if [ $1 == 'coref_nofinetune_layers' ]
-then
-coref_nofinetune_layers
-fi
-
-if [ $1 == 'rel_nofinetune_layers' ]
-then
-rel_nofinetune_layers
-fi
-
-if [ $1 == 'qtype_babi_layers' ]
-then
-qtype_babi_layers
+ner_nofinetune_large
 fi
 
 if [ $1 == 'sup_babi2_babi' ]
 then
 sup_babi2_babi
+fi
+
+### to be done
+
+if [ $1 == 'ner_babi' ]
+then
+ner_babi
+fi
+
+if [ $1 == 'qtype_hotpot' ]
+then
+qtype_hotpot
+fi
+
+if [ $1 == 'qtype_nofinetune_base' ]
+then
+qtype_nofinetune_base
+fi
+
+if [ $1 == 'qtype_nofinetune_large' ]
+then
+qtype_nofinetune_large
+fi
+
+if [ $1 == 'sup_babi2_hotpot' ]
+then
+sup_babi2_hotpot
+fi
+
+if [ $1 == 'sup_babi2_nofinetune_base' ]
+then
+sup_babi2_nofinetune_base
+fi
+
+if [ $1 == 'sup_babi2_nofinetune_large' ]
+then
+sup_babi2_nofinetune_large
+fi
+
+if [ $1 == 'rel_nofinetune_base' ]
+then
+rel_nofinetune_base
+fi
+
+if [ $1 == 'ner_nofinetune_base' ]
+then
+ner_nofinetune_base
+fi
+
+if [ $1 == 'coref_nofinetune_base' ]
+then
+coref_nofinetune_base
 fi
 
